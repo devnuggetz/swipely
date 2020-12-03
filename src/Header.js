ba255919130c586cc762e213837fddf3baa72f00
shuvamk <kshuvam564@gmail.com>
2020-12-03 09:38:14 -0800

    Added the header icons

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import { IconButton } from "@material-ui/core";
+import logo from "./assets/logo.png";
+import ForumIcon from "@material-ui/icons/Forum";
 
 function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+      <img className="header__logo" src={logo} alt="logo" />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
+    </div>
+  );
 }
 
 export default Header;
